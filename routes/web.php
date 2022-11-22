358d764c2ef88a5605022d16a55ccb87a2c750e4
Felipe Gomez <kaboom300@gmail.com>
2022-11-22 09:26:49 +0100

    Layout y primera vista

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/encuentro', function () {
+Route::get('encuentro', function () {
     return view('encuentro');
+});
+
+Route::get('contact', function () {
+    return view('contact');
 });
