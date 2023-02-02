eb8c6664891a32f5d16e1151c251871ef5d29a57
Miguel <733350@alu.murciaeduca.es>
2023-02-02 13:15:16 +0100

    polices creadas

--- app/Http/Controllers/API/CustomerController.php
+++ app/Http/Controllers/API/CustomerController.php
@@ -10,6 +10,15 @@ use App\Http\Resources\CustomerResource;
 
 class CustomerController extends Controller
 {
+        /**
+     * Create the controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->authorizeResource(Customer::class, 'customer');
+    }
     /**
      * Display a listing of the resource.
      *
