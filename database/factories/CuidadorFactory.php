aaf372fbd6bfdf44f7cdb966701351b586d32403
JaimeLloretGarcia <9773471@alu.murciaeduca.es>
2023-02-24 10:24:46 +0100

    Factory

--- database/factories/CuidadorFactory.php
+++ database/factories/CuidadorFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\User;
 
 class CuidadorFactory extends Factory
 {
@@ -14,15 +15,19 @@ class CuidadorFactory extends Factory
     public function definition()
     {
         //Factory hecha, pero no podemos utilizarla.
+
+        //$user = User::factory()->create();
         $this->faker = \Faker\Factory::create('es_ES');
         return [
+            //'user_id'=>$user->id,
             'nombre' => $this->faker->firstName(),
             'apellidos' => $this->faker->lastName(),
-            'telefono' => $this->faker->phoneNumber(),
+            'telefono' => $this->faker->numerify('#########'),
             'dni' => $this->faker->regexify('/^[0-9]{8}[A-Z]$/'),
             'email' => $this->faker->email(),
             'Domicilio' => $this->faker->word(),
             'Comunidad' => $this->faker->country(),
+
         ];
     }
 }
